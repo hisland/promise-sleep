483f3fda8f3388b080dbd4487232b3c659f9c02a
hisland <hisland@qq.com>
2018-11-07 10:22:48 +0800

    refactor: update test

--- index.test.js
+++ index.test.js
@@ -7,17 +7,25 @@ test('has sleep fn', done => {
 })
 
 test('resolve will sleep', done => {
+  const t1 = new Date()
   Promise.resolve('foo')
     .sleep(500)
-    .then(() => {
+    .then(rs1 => {
+      const t2 = new Date()
+      expect(t2 - t1 >= 500).toBe(true)
+      expect(rs1).toBe('foo')
       done()
     })
 })
 
 test('reject immediately', done => {
+  const t1 = new Date()
   Promise.reject('bar')
     .sleep(1000)
-    .then(null, () => {
+    .then(null, err => {
+      const t2 = new Date()
+      expect(t2 - t1 < 10).toBe(true)
+      expect(err).toBe('bar')
       done()
     })
 })
